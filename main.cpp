2e8b0a8c9f2e9a5847b5038c71fa089ead4fdcf6
JasonWQWu <123336977+JasonWQWu@users.noreply.github.com>
2023-04-21 01:35:42 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+
+    std::ifstream data;
+    std::string line;
+
+    data.open(argv[1]);
+    while(std::getline(data,line)) {
+        std::string entry, time, value;
+        std::istringstream iss(line);
+        iss >> time >> value;
+
+        // Made node with time and value
+        // Append to array
+        // Make tree
+    }
+}
